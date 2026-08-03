0c00a94206efe07cffcbd0db5b54a381f3376685
chsumin0316-oss <chsumin0316@gmail.com>
2026-08-03 18:02:04 +0900

    Add files via upload

---
+++ sumin/main.py
@@ -0,0 +1,73 @@
+from http.client import responses
+
+from schema.response import TodoResponse #응답 모델 불러오기
+from schema.request import TodoCreateRequest, TodoUpdateRequest #생성 요청 모델 불러오기
+from fastapi import FastAPI, status, HTTPException
+
+app = FastAPI()
+#할 일 저장
+todos = [
+    {"id": 1, "title": "FastAPI 공부하기", "is_done": False},
+    {"id": 2, "title": "운동하기", "is_done": True},
+    {"id": 3, "title": "책 읽기", "is_done": False}
+]
+#전체 할 일 조회
+@app.get(
+    "/todos",
+    response_model=list[TodoResponse], #TOdoResponse 객체로 이루어진 리스트 반환
+    status_code=status.HTTP_200_OK #데이터 조회 요청이 성고했을 때의 상태 코드 반환
+)
+#/todos 경로로 데이터 조회 요청시 아래 함수를 실행
+def get_todos_handler():
+    return todos
+
+@app.get(
+    "/todos/{todo_id}", #{todo_id}는 경로 변수로, 경로변수에 정수 값을 넣으면 todo_id에 저장되어 아래 함수의 매개변수로 전달
+    response_model=TodoResponse,
+    status_code=status.HTTP_200_OK #데이터 조회 요청이 성고했을 때의 상태 코드 반환
+)
+def get_todo_handler(todo_id: int): #todo_id를 int로 정의하여 FastAPI는 전달된 값이 정수인지 자동 검증
+    for todo in todos:
+        if todo["id"] == todo_id:
+            return todo
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+@app.post(
+    "/todos",
+    response_model=TodoResponse,
+    status_code=status.HTTP_201_CREATED
+)
+def create_todo_handler(body: TodoCreateRequest): #요청 본문을 body 매개변수로 받고 타입을 TodoCreateRequest로 지정
+    new_todo = { #새 할일 데이터 생성하고 new_todo에 저장
+        "id": len(todos) +1,
+        "title": body.title,
+        "is_done": body.is_done,
+    }
+    todos.append(new_todo) #new_todo를 todos 리스트에 추가
+    return new_todo #생성된 데이터를 응답으로 반환
+
+@app.patch(
+    "/todos/{todo_id}",
+    response_model=TodoResponse,
+    status_code=status.HTTP_200_OK
+)
+def update_todo_handler(todo_id: int, body: TodoUpdateRequest):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            if body.title is not None:
+                todo["title"] = body.title
+            if body.is_done is not None:
+                todo["is_done"] = body.is_done
+            return todo
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+@app.delete(
+    "/todos/{todo_id}",
+    status_code=status.HTTP_204_NO_CONTENT
+)
+def delete_todo_handler(todo_id: int):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            todos.remove(todo)
+            return
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
